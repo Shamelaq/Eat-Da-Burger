5979ab24b2ab786fae4c75ecce9c9334d7f2e3b8
Shamelaq <qazeishamela@gmail.com>
2017-12-28 15:02:34 -0800

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whine Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Crispy Chicken Burger', true);
